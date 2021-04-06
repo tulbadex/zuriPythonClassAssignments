af99fec71d017c301b0074342ff41d2275b4c646
tulbadex <ibrahimadedayo@rocketmail.com>
2021-04-06 08:59:52 -0700

    Added function to project

---
+++ functions.py
@@ -0,0 +1,10 @@
+def functionName(name, count):
+    print('%s call function with %d' % (name, count))
+
+functionName('seyi', 4)
+
+def functionName1(name, count):
+    print(name * count)
+
+functionName1('seyi ', 4)
+functionName1('ibro ', 8)
---
+++ ifandwhileloop.py
@@ -0,0 +1,21 @@
+# if else 
+alist = ['Mike', 'Live', 'Long']
+name = 'Live'
+
+if name in alist:
+    print('Name Found')
+else:
+    print('Name not Found')
+
+# for loop
+for x in range(6):
+    if not(x==0):
+        print(x)
+
+# While loop
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+
